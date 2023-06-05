31a54525e5c54aa124240262fcf2cbaa0a815720
Pijush Sarker <pijushsarker.dev@gmail.com>
2023-06-05 23:08:43 +0600

    stripe payment gateway

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,8 +1,28 @@
+import { Helmet } from "react-helmet-async";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckOutForm from "./CheckOutForm";
+import useCart from "../../../hooks/useCart";
+
+const payment_api_key = import.meta.env.VITE_Payment_Gateway_PK;
+
+const stripePromise = loadStripe(payment_api_key);
 
 const Payment = () => {
+    const [cart] = useCart();
+    const total = cart.reduce((sum, item) => sum + item.price, 0);
+    const price = parseFloat(total.toFixed(2));
+
+
     return (
         <div>
-            
+            <Helmet><title>Bistro Boss || Payment</title></Helmet>
+            <SectionTitle heading='Make Payment' subHeading='Happy Shopping'></SectionTitle>
+
+            <Elements stripe={stripePromise}>
+                <CheckOutForm cart={cart} price={price} />
+            </Elements>
         </div>
     );
 };
